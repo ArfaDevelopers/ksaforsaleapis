e6469a0ab6bc784f846a8d89142867932375abb3
ArfaDevelopers <aarfa.developers@gmail.com>
2025-03-13 01:28:51 +0500

    sas

--- app.js
+++ app.js
@@ -9,14 +9,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(require("./middleware/error.js"));
 app.use(compression());
 
 app.get("/", (_, res) => {
   return res.send("<a href='/route'>Click to redirect to /route</a>");
 });
 
-app.use("/route", require("./routes/route"));
+app.use("/route", require("./routes/route")); // Includes Twilio OTP + Firestore Cars
 
 const PORT = process.env.PORT || 9002;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
