df1e730c5abd217c740e1447a27326c72de966ff
ArfaDevelopers <aarfa.developers@gmail.com>
2025-05-23 21:38:23 +0500

    as

--- firebase/config.js
+++ firebase/config.js
@@ -1,11 +1,11 @@
 const admin = require("firebase-admin");
 
-// const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = require("./serviceAccountKey.json");
 
-// admin.initializeApp({
-//   credential: admin.credential.cert(serviceAccount),
-//   databaseURL: "https://ksa4sale-classified.firebaseio.com", // Optional for Firebase Realtime Database
-// });
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://ksa4sale-classified.firebaseio.com", // Optional for Firebase Realtime Database
+});
 
 const db = admin.firestore(); // Firestore initialization
 const storage = admin.storage(); // Storage initialization (if you need it)
