9faa31625edbcaf9ba7b51f992fdc05acc522e5e
ArfaDevelopers <aarfa.developers@gmail.com>
2025-05-23 22:20:51 +0500

    as

--- firebase/config.js
+++ firebase/config.js
@@ -1,13 +1,38 @@
+// const admin = require("firebase-admin");
+
+// const serviceAccount = require("./serviceAccountKey.json");
+
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   databaseURL: "https://ksa4sale-classified.firebaseio.com", // Optional for Firebase Realtime Database
+// });
+
+// const db = admin.firestore(); // Firestore initialization
+// const storage = admin.storage(); // Storage initialization (if you need it)
+
+// module.exports = { admin, db, storage };
+require("dotenv").config();
 const admin = require("firebase-admin");
 
-const serviceAccount = require("./serviceAccountKey.json");
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n"),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_X509_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://ksa4sale-classified.firebaseio.com", // Optional for Firebase Realtime Database
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
 
-const db = admin.firestore(); // Firestore initialization
-const storage = admin.storage(); // Storage initialization (if you need it)
+const db = admin.firestore();
+const storage = admin.storage();
 
 module.exports = { admin, db, storage };
