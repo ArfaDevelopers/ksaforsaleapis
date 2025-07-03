8fb73c567642253f50ca256c52067b62878df252
AsifShabir786 <buttehtesham86@gmail.com>
2025-07-03 19:04:17 +0500

    Update app.js only

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const createError = require("http-errors");
 const admin = require("firebase-admin");
 const path = require("path");
 const fs = require("fs");
+const bookmarkedListingsRouter = require("./routes/bookmarkedListings");
+
 const listingsRoute = require("./routes/listings"); // ✅ path to your listings route
 
 const { getFirestore } = require("firebase-admin/firestore");
@@ -55,6 +57,8 @@ app.get("/", (_, res) => {
 app.use("/api", listingsRoute); // your route is now GET /api/listings
 
 app.use("/route", require("./routes/route")); // Includes Twilio OTP + Firestore Cars
+app.use("/api", bookmarkedListingsRouter); // So route is at /api/bookmarked-listings
+
 const getOrCreateChat = async (sender, receiver) => {
   try {
     const chatRef = db.collection("chats");
